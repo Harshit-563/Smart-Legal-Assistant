46630f5f68688c61d5e6681de5769850084bdf18
Harshit Maurya <harsitmaurya555@gmail.com>
2026-01-18 14:09:36 +0530

    cors update

--- src/smart_legal_assistant.py
+++ src/smart_legal_assistant.py
@@ -28,6 +28,9 @@ app = FastAPI(title="Smart Legal Assistant - Prototype")
 origins = [
     "https://smart-legal-assistant-bice.vercel.app",
     "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
